27dbbabbb4e17f207b8b6b4a7edcad27a160981f
Dusan Matejka <matejkadusan32@gmail.com>
2015-02-05 19:19:11 +0100

    routed api_haproxy

--- eszone_haproxy/urls.py
+++ eszone_haproxy/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from settings import API_VERSION
+from settings import API_VERSION_PREFIX
 
 urlpatterns = [
-    url(r'^{0}/'.format(API_VERSION), include('api_core.urls')),
+    url(r'^{0}/'.format(API_VERSION_PREFIX), include('api_core.urls')),
+    url(r'^{0}/haproxy/'.format(API_VERSION_PREFIX), include('api_haproxy.urls'))
 ]
